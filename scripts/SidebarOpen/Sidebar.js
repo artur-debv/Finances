813e22229cd87e09ad696b74f0b3c1df176c2fc2
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-09-01 19:18:37 -0300

    feat: I created the functionality to open the sidebar when clicking on the menubar

--- scripts/SidebarOpen/Sidebar.js
+++ scripts/SidebarOpen/Sidebar.js
@@ -1,5 +1,6 @@
 const Menu = document.getElementById("icon_menuBar")
 const Button_Close = document.getElementById("open_btn")
+const Sidebar = document.getElementById("sidebar")
 
 Menu.addEventListener("click",()=>{
   console.log("clicou")
