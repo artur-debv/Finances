b259c38d5b73c2e53f430e4566e7230feb6f894a
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-09-05 21:42:47 -0300

    feat: I added the functionality to close sidebar when clicking on the mobile screen

--- scripts/SidebarOpen/Sidebar.js
+++ scripts/SidebarOpen/Sidebar.js
@@ -2,7 +2,7 @@ const Menu = document.getElementById("icon_menuBar");
 const Button_Close = document.getElementById("open_btn");
 const Sidebar = document.getElementById("sidebar");
 
-// Abre a sidebar ao clicar no ícone do menu
+
 Menu.addEventListener("click", () => {
     Sidebar.classList.add('open-sidebar');
     Sidebar.style.display = "flex";
@@ -11,12 +11,12 @@ Menu.addEventListener("click", () => {
 });
 
 document.addEventListener("click", (event) => {
-    if (window.innerWidth <= 900) {  // Verifica se a largura da tela é menor ou igual a 768px
-        // Verifica se o clique foi fora do sidebar e do menu
+    if (window.innerWidth <= 900) {  
+        
         if (!Sidebar.contains(event.target) && !Menu.contains(event.target)) {
             Sidebar.classList.remove('open-sidebar'); // Remove a classe
 
-            // Reseta os estilos
+            
             Sidebar.style.display = "none";
             Sidebar.style.width = "0";
         }
