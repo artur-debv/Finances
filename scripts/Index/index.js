9c7bc20ee7a30b56a4b47f0945dd2d4f750daf78
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-09-07 23:01:39 -0300

    feat: I added formatting to the index javascript code

--- scripts/Index/index.js
+++ scripts/Index/index.js
@@ -10,23 +10,23 @@ function parseJwt(token) {
 }
 
 if (!localStorage.getItem('jwtToken')) {
-    // Redireciona para a página de login se não houver um JWT válido
-  window.location.href = '/Form.html';
+
+    window.location.href = '/Form.html';
 } else {
 
 
     window.onload = function () {
         const jwtToken = localStorage.getItem('jwtToken');
-       
+
         const decodedToken = parseJwt(jwtToken);
 
-        if(decodedToken){
+        if (decodedToken) {
             UpdateData(decodedToken.name, decodedToken.email, decodedToken.picture)
-        }else{
+        } else {
             console.log('erro')
         }
-        
-   
+
+
     }
 
 
@@ -43,22 +43,22 @@ if (!localStorage.getItem('jwtToken')) {
 
     }
 }
-// Função para obter parâmetros de query string da URL
+
 function UpdateData(name, email, pictureUrl) {
     document.querySelector('.item-description').innerHTML = name
     avatarElement.src = pictureUrl;
 }
 
-// Obter o valor do parâmetro 'picture' da URL
+
 const pictureUrl = getParameterByName('picture');
 
-// Selecionar o elemento com o id 'user_avatar'
+
 const avatarElement = document.getElementById('user_avatar');
 
-// Definir o atributo src do elemento <img> com a URL da imagem
+
 avatarElement.src = pictureUrl;
 
-// Obter os valores dos parâmetros de query string
+
 const name = getParameterByName('name');
 const email = getParameterByName('email');
 
@@ -67,7 +67,7 @@ UpdateData(name, email, pictureUrl)
 var newUrl = window.location.pathname;
 window.history.pushState({}, '', newUrl);
 
-// Selecionar elementos do DOM
+
 const menuBar = document.getElementById('menu-bar');
 const menuIcon = document.getElementById('menu-icon');
 const sidebar = document.getElementById('sidebar');
