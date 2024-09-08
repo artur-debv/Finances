1a788737c969c9143a48fde4055915614fea8d15
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-09-07 23:03:05 -0300

    feat: I added formatting to the sidebar javascript code

--- scripts/Sidebar/sidebar.js
+++ scripts/Sidebar/sidebar.js
@@ -2,9 +2,9 @@ document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
 
- // Função para desconectar o usuário da conta do Google
- function signOut() {
+
+function signOut() {
     google.accounts.id.disableAutoSelect();
-    // Redirecione para a página de login ou faça outras ações de logout, se necessário
-    window.location.href = "https://seusite.com/form.html"; // Altere para o URL correto da página de login
+
+    window.location.href = "https://seusite.com/form.html";
 }
