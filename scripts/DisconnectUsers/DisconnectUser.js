ce17b48858f94d84346d00e033609b5e90a4043a
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2025-03-10 22:19:08 -0300

    feat: troquei a url de redirecionamento quando clicar em sair do sistema

--- scripts/DisconnectUsers/DisconnectUser.js
+++ scripts/DisconnectUsers/DisconnectUser.js
@@ -1,16 +1,11 @@
 function disconnectUser() {
     localStorage.removeItem('jwtToken');
-    // Verifica se a biblioteca está carregada
     if (typeof google.accounts !== 'undefined' && google.accounts.id) {
         try {
-
             google.accounts.id.revoke();
-
-            // Redireciona para a página desejada após a desconexão (opcional)
-            window.location.href = "https://finances-two-inky.vercel.app/Form.html";
+            window.location.href = "https://finances-three-rose.vercel.app/Form.html";
         } catch (error) {
             console.error('Erro ao revogar o token de autenticação:', error);
-            // Trate o erro aqui, se necessário
         }
     } else {
         console.error('Biblioteca google.accounts não carregada corretamente.');
